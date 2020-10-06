faaab56af397f02c628165689db5b69ec62edaa8
Catuntu <chshih2@illinois.edu>
2020-10-06 13:55:36 -0500

    add load function for continue training

--- main.py
+++ main.py
@@ -19,6 +19,22 @@ from a2c_ppo_acktr.model import Policy
 from a2c_ppo_acktr.storage import RolloutStorage
 from evaluation import evaluate
 
+def load_checkpoint(model, optimizer, filename='checkpoint.pth.tar'):
+    # Note: Input model & optimizer should be pre-defined.  This routine only updates their states.
+    start_epoch = 0
+    if os.path.isfile(filename):
+        print("=> loading checkpoint '{}'".format(filename))
+        checkpoint = torch.load(filename)
+        start_epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(filename, checkpoint['epoch']))
+    else:
+        print("=> no checkpoint found at '{}'".format(filename))
+
+    return model, optimizer, start_epoch
+
 
 def main():
     args = get_args()
@@ -46,6 +62,10 @@ def main():
         envs.action_space,
         base_kwargs={'recurrent': args.recurrent_policy})
     actor_critic.to(device)
+
+    save_path = os.path.join(args.save_dir, args.algo)
+
+
 
     if args.algo == 'a2c':
         agent = algo.A2C_ACKTR(
@@ -79,7 +99,7 @@ def main():
         file_name = os.path.join(
             args.gail_experts_dir, "trajs_{}.pt".format(
                 args.env_name.split('-')[0].lower()))
-        
+
         expert_dataset = gail.ExpertDataset(
             file_name, num_trajectories=4, subsample_frequency=20)
         drop_last = len(expert_dataset) > args.gail_batch_size
@@ -88,6 +108,22 @@ def main():
             batch_size=args.gail_batch_size,
             shuffle=True,
             drop_last=drop_last)
+
+    if args.load:
+        # actor_critic,ob_rms2=torch.load(os.path.join(save_path, args.env_name + ".pt"))
+        # evaluate(actor_critic, ob_rms2, args.env_name, args.seed,
+        #              args.num_processes, eval_log_dir, device)
+        #actor_critic.eval()
+        #exit()
+        #.state_dict()
+        actor_critic, agent.optimizer, start_epoch = load_checkpoint(actor_critic, agent.optimizer,os.path.join(save_path, args.env_name + ".pt"))
+        actor_critic = actor_critic.to(device)
+        for state in agent.optimizer.state.values():
+            for k, v in state.items():
+                if isinstance(v, torch.Tensor):
+                    state[k] = v.to(device)
+    else:
+        start_epoch=0
 
     rollouts = RolloutStorage(args.num_steps, args.num_processes,
                               envs.observation_space.shape, envs.action_space,
@@ -99,10 +135,11 @@ def main():
 
     episode_rewards = deque(maxlen=10)
 
+    # ob_rms=ob_rms2
     start = time.time()
     num_updates = int(
         args.num_env_steps) // args.num_steps // args.num_processes
-    for j in range(num_updates):
+    for j in range(start_epoch,num_updates):
 
         if args.use_linear_lr_decay:
             # decrease learning rate linearly
@@ -164,16 +201,18 @@ def main():
         # save for every interval-th episode or for the last epoch
         if (j % args.save_interval == 0
                 or j == num_updates - 1) and args.save_dir != "":
-            save_path = os.path.join(args.save_dir, args.algo)
             try:
                 os.makedirs(save_path)
             except OSError:
                 pass
+            state = {'epoch': j + 1, 'state_dict': actor_critic.state_dict(),
+             'optimizer': agent.optimizer.state_dict()}
+            torch.save(state, os.path.join(save_path, args.env_name + ".pt"))
+            # torch.save([
+            #     actor_critic,
+            #     getattr(utils.get_vec_normalize(envs), 'ob_rms', None)
+            # ], os.path.join(save_path, args.env_name + ".pt"))
 
-            torch.save([
-                actor_critic,
-                getattr(utils.get_vec_normalize(envs), 'ob_rms', None)
-            ], os.path.join(save_path, args.env_name + ".pt"))
 
         if j % args.log_interval == 0 and len(episode_rewards) > 1:
             total_num_steps = (j + 1) * args.num_processes * args.num_steps
